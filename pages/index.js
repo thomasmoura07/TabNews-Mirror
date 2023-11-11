5e1b94f7cedef42aeeec35f0d65059c5f800fc81
thomasmoura07 <99275405+thomasmoura07@users.noreply.github.com>
2023-11-11 14:02:42 +0000

    scripts added: `lint:check` and `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-function Home(){
-    return <h1>Bem vindo! Por enquanto tudo é mato...Mas é um início que quero compartilhar com você!🥰</h1>
+function Home() {
+  return (
+    <h1>
+      Bem vindo! Por enquanto tudo é mato...Mas é um início que quero
+      compartilhar com você!🥰
+    </h1>
+  );
 }
 
 export default Home;
